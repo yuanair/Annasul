39436c48d3ea092a2eab0b6e58e166d7011bae19
Xinbang Hu <139057795+yuanair@users.noreply.github.com>
2024-10-19 18:27:45 +0800

    Transform

--- Annasul/Source/Runtime/Engine/Public/SceneComponent.hpp
+++ Annasul/Source/Runtime/Engine/Public/SceneComponent.hpp
@@ -2,19 +2,21 @@
 
 #include "Platform.hpp"
 #include "ActorComponent.hpp"
+#include "Transform.hpp"
 
 namespace Annasul
 {
 	
-	class SCSceneComponent : public ACActorComponent
-	{
+	class SCSceneComponent : public ACActorComponent {
 	public:
 		
 		FORCEINLINE SCSceneComponent() = default;
+		
 		FORCEINLINE ~SCSceneComponent() override = default;
 	
 	private:
-	
+		
+		FTransform transform;
 		
 	};
 	
--- Annasul/Source/Runtime/Math/Public/Transform.hpp
+++ Annasul/Source/Runtime/Math/Public/Transform.hpp
@@ -9,6 +9,19 @@ namespace Annasul
 	class FTransform3D {
 	public:
 		
+		static const FTransform3D Identity;
+	
+	public:
+		
+		FTransform3D() {}
+		
+		~FTransform3D() = default;
+	
+	public:
+	
+	
+	protected:
+		
 		VectorRegister4<InElementType> Rotation;
 		VectorRegister4<InElementType> Translation;
 		VectorRegister4<InElementType> Scale;
